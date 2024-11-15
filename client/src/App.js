ea39afc999136ecb26b17d02ef50a0b1611fe8de
George Xie <georgedontspam@gmail.com>
2024-11-15 03:29:19 -0500

    Setting up Blog server

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+        <div className="post"></div>
+          <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/toronto/167_3_4408_jpeg_ba9e8fd7-3bf6-4b5b-8eca-9a93e5c97a5b.jpg" alt="" />
+          <h2>Christmas market is back in the Distillery District</h2>
+          <p>Love it or hate it, the time has come once again. The Distillery District Winter Village is back for 2024, and it'll be taking over the historic district and surrounding areas 7 days a week until Jan. 5, 2025. Ho ho ho!</p>
+    </main>
+  );
+}
+
+export default App;
