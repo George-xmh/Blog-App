ce3be01b3d39d65cb00d823e9c337bc48a50931d
George Xie <xiex4179@mylaurier.ca>
2024-11-15 05:01:55 -0500

    Routing Sections
    Routing my sources with React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
-
+import Post from "./Post";
+import Header from "./Header";
+import {Routes, Route} from "react-router-dom"
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-        <div className="post"></div>
-          <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/toronto/167_3_4408_jpeg_ba9e8fd7-3bf6-4b5b-8eca-9a93e5c97a5b.jpg" alt="" />
-          <h2>Christmas market is back in the Distillery District</h2>
-          <p>Love it or hate it, the time has come once again. The Distillery District Winter Village is back for 2024, and it'll be taking over the historic district and surrounding areas 7 days a week until Jan. 5, 2025. Ho ho ho!</p>
-    </main>
+    <Routes>
+      <Route index element={
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      } />
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div className="post">
+          <div className="image">
+            <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/toronto/167_3_4408_jpeg_ba9e8fd7-3bf6-4b5b-8eca-9a93e5c97a5b.jpg" alt="" />
+          </div>
+          <div className="title">
+            <h2>Christmas market is back in the Distillery District</h2>
+            <p className="info">
+              <a className="author">George Xie</a>
+              <time >2024-11-15 4:25 AM</time>
+            </p>
+            <p className='summary'>Love it or hate it, the time has come once again. The Distillery District Winter Village is back for 2024, and it'll be taking over the historic district and surrounding areas 7 days a week until Jan. 5, 2025. Ho ho ho!</p>
+        </div>
+      </div>
+    );
+}
